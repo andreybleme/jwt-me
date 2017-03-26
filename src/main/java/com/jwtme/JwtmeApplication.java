db6ea9b765de3db0acb15ca40f7f6c5ec37e3bbd
andreybleme <andreybleme1@gmail.com>
2017-03-26 20:12:33 -0300

    Initial commit

---
+++ src/main/java/com/jwtme/JwtmeApplication.java
@@ -0,0 +1,12 @@
+package com.jwtme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtmeApplication.class, args);
+	}
+}
