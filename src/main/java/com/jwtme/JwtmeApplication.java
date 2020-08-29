83288bd7846dcd211325f3e168b2682125473805
Lucas Batista <lucasdanbatista@hotmail.com>
2020-08-29 11:30:36 -0300

    Remove anotação redundante

--- src/main/java/com/jwtme/JwtmeApplication.java
+++ src/main/java/com/jwtme/JwtmeApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class JwtmeApplication {
 
 	public static void main(String[] args) {
